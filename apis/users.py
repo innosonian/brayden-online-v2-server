c6b2cb56efa5cfab0484c49f32c7ab5b1e8ee89f
david-innosonian <david@innosonian.com>
2024-03-11 09:26:24 +0900

    fix error can't return user info there is no user.training_result yet.

--- apis/users.py
+++ apis/users.py
@@ -11,32 +11,34 @@ from database import get_db
 router = APIRouter(prefix="/users")
 per_page = 10
 
+
 @router.get('/{user_id}')
 async def get_user(user_id: int, db: Session = Depends(get_db)):
-    #options(joinedload(User.training_result))
+    # options(joinedload(User.training_result))
     query = select(User).where(User.id == user_id)
-    user = db.execute(query).unique().scalar()
+    user = db.execute(query).scalar()
     if not user:
         return None
 
-    if user.training_result:
-        date = user.training_result.pop().date
-    else:
-        date = None
-    return {
-        "name": user.name,
-        "email": user.email,
-        "employee_id": user.employee_id,
-        "last_training_date": date,
-        "certifications": {
-            "adult": {
-                "expiration": "2023-11-23"
-            },
-            "child": {
-                "expiration": None
-            },
-            "baby": {
-                "expiration": "2025-11-23"
-            }
-        }
-    }
+    return user
+    # if user.training_result:
+    #     date = user.training_result.pop().date
+    # else:
+    #     date = None
+    # return {
+    #     "name": user.name,
+    #     "email": user.email,
+    #     "employee_id": user.employee_id,
+    #     "last_training_date": date,
+    #     "certifications": {
+    #         "adult": {
+    #             "expiration": "2023-11-23"
+    #         },
+    #         "child": {
+    #             "expiration": None
+    #         },
+    #         "baby": {
+    #             "expiration": "2025-11-23"
+    #         }
+    #     }
+    # }
