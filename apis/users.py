691f7505dca4b128919a1bfab320d38be13d9099
david-innosonian <david@innosonian.com>
2024-03-11 10:57:54 +0900

    add get users api append filtered data

--- apis/users.py
+++ apis/users.py
@@ -4,12 +4,39 @@ from fastapi import APIRouter, Depends
 from models.model import User
 
 from sqlalchemy.orm import Session, joinedload
-from sqlalchemy import select
+from sqlalchemy import select, or_, func
 
 from database import get_db
 
 router = APIRouter(prefix="/users")
 per_page = 10
+@router.get('', status_code=status.HTTP_200_OK)
+async def get_users(page: int = 1, search_keyword: str = None, db: Session = Depends(get_db)):
+    def get_users_by_search_keyword(search_keyword):
+        def all_users(limit: int, offset: int = 0):
+            query = select(User).order_by(User.id.desc())
+            return db.scalars(query.offset(offset).limit(limit)).all(), query
+
+        def filtered_users(search_keyword, limit: int, offset: int = 0):
+            query = (select(User).where(or_(User.email.contains(search_keyword),
+                                            User.employee_id.contains(search_keyword),
+                                            User.name.contains(search_keyword))).order_by(User.id.desc()))
+            return db.scalars(query.limit(limit).offset(offset)).all(), query
+
+        offset = (page - 1) * per_page
+        limit = page * per_page
+
+        if search_keyword:
+            users, query = filtered_users(search_keyword, limit, offset)
+        else:
+            users, query = all_users(limit, offset)
+
+        # all user count
+        filtered_data_count = db.scalar(select(func.count('*')).select_from(query))
+
+        return {"users": users, "total": filtered_data_count, "per_page": per_page, "current_page": page}
+
+    return get_users_by_search_keyword(search_keyword)
 
 
 @router.get('/{user_id}')
