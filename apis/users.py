99061539f16e390d2c0ffc30b8f92dc02779d025
david-innosonian <david@innosonian.com>
2024-03-11 15:34:48 +0900

    implement user create from xlsx file - upload fail reason file to s3 - return s3 url - TODO change each insert to.insert bulk logic

--- apis/users.py
+++ apis/users.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 import regex
 from fastapi import APIRouter, Depends, status, HTTPException, Request, UploadFile
@@ -7,16 +8,21 @@ from exceptions import GetException, ExceptionType, GetExceptionWithStatuscode
 from models.model import User
 
 from sqlalchemy.orm import Session, joinedload
-from sqlalchemy import select, or_, func
+from sqlalchemy import select, or_, func, insert
 
 from bcrypt import hashpw
 
 from database import get_db
 from schema import UserCreateResponseSchema, UserCreateRequestSchema, UserUpdateRequestSchema
 
+from pandas import read_excel, DataFrame
+
+from boto3 import client
+
 router = APIRouter(prefix="/users")
 per_page = 10
 salt = b'$2b$12$apcpayF3r/A/kKo2dlRk8O'
+BUCKET_NAME = 'brayden-online-v2-api-storage'
 
 
 def get_token_by_header(headers):
@@ -68,6 +74,138 @@ async def create_user(request: Request, user: UserCreateRequestSchema, db: Sessi
     db.refresh(insert_user)
 
     return insert_user
+
+
+def validate_file_format(upload_file: UploadFile):
+    # validate file format
+    if '[Content_Types].xml'.encode('utf-8') not in upload_file.file.read():
+        raise GetExceptionWithStatuscode(status.HTTP_400_BAD_REQUEST,
+                                         'incorrect file format',
+                                         ExceptionType.INCORRECT_FORMAT)
+
+    upload_file.file.seek(0)
+
+
+def validate_email(email):
+    if not regex.match(r"^\S+@\S+$", email):
+        return False
+    return True
+
+
+def validate_file_extension(name: str):
+    split_name = name.split(".")
+    if split_name[1] != "xlsx":
+        raise GetExceptionWithStatuscode(status.HTTP_400_BAD_REQUEST,
+                                         'incorrect file format',
+                                         ExceptionType.INCORRECT_FORMAT)
+
+
+def authorize_aws_s3():
+    if os.environ.get('aws_access_key_id') and os.environ.get('aws_secret_access_key'):
+        access_key = os.environ.get('aws_access_key_id')
+        secret_access_key = os.environ.get('aws_secret_access_key')
+        s3 = client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_access_key)
+    else:
+        s3 = client('s3')
+    return s3
+
+
+def upload_excel_to_s3(file):
+    s3 = authorize_aws_s3()
+    buckets = [i['Name'] for i in s3.list_buckets()['Buckets']]
+    if BUCKET_NAME in buckets:
+        ret = s3.upload_fileobj(file, BUCKET_NAME, file.name)
+        location = s3.get_bucket_location(Bucket=BUCKET_NAME)['LocationConstraint']
+        return location
+    return None
+
+
+def get_presigned_url_from_upload_file(filename):
+    s3 = authorize_aws_s3()
+    return s3.generate_presigned_url('get_object', Params={'Bucket': BUCKET_NAME, 'Key': filename}, ExpiresIn=3600)
+
+
+@router.post('/upload')
+async def user_upload(request: Request, file: UploadFile, db: Session = Depends(get_db)):
+    file_name = 'user_upload_fail_with_reason.xlsx'
+    try:
+        token = get_token_by_header(request.headers)
+        me = get_user_by_token(token, db)
+        validate_file_extension(file.filename)
+        validate_file_format(file)
+    except GetExceptionWithStatuscode as e:
+        if e.exception_type == ExceptionType.INCORRECT_FORMAT:
+            logging.error(e)
+            raise HTTPException(e.status_code, detail=e.message)
+        elif e.exception_type == ExceptionType.INVALID_TOKEN:
+            logging.error(e)
+            raise HTTPException(e.status_code, e.message)
+    except GetException as e:
+        if e.exception_type == ExceptionType.INVALID_TOKEN:
+            logging.error(e)
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=e.message)
+        elif e.exception_type == ExceptionType.NOT_FOUND:
+            logging.error(e)
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=e.message)
+
+    organization_id = me.organization_id
+
+    users = read_excel(file.file.read())
+    failure_users = DataFrame()
+    failure_count = 0
+    success_count = 0
+    # 삽입
+    for i, r in users.iterrows():
+        user = {}
+        # DB에 있는 이메일과 중복되는지 확인
+        # TODO bulk로 넣고 실패한 걸 체크하는 방향으로 가야함.
+        if db.scalar(select(User).where(User.email == r['email'])):
+            failure_count += 1
+            r['reason'] = 'email duplicated'
+            failure_users = failure_users._append(r, ignore_index=True)
+            continue
+        if not validate_email(r['email']):
+            failure_count += 1
+            r['reason'] = 'email invalid'
+            failure_users = failure_users._append(r, ignore_index=True)
+            continue
+
+        for k, v in r.items():
+            if k == 'password':
+                v = hashpw(v.encode('utf-8'), salt).decode('utf-8')
+                k = "password_hashed"
+                user['organization_id'] = organization_id
+            user[k] = v
+
+        ret = db.execute(insert(User).values(user))
+        if ret.rowcount == 1:
+            success_count += 1
+        else:
+            failure_count += 1
+            r['reason'] = 'insert fail'
+            failure_users = failure_users._append(r, ignore_index=True)
+
+    def make_excel_data_from_dataframe(failure_users, file_name):
+        failure_users.to_excel(file_name)
+
+    # 실패 사유가 담긴 파일 만들기
+    url = None
+    if failure_count > 0:
+        failure_users.pop('employee_id')
+        failure_users.pop('password')
+        # 파일 생성
+        make_excel_data_from_dataframe(failure_users, file_name)
+        with open(file_name, 'rb') as f:
+            # 파일 업로드
+            if upload_excel_to_s3(f):
+                # 파일 링크 얻어오기
+                url = get_presigned_url_from_upload_file(f.name)
+        # 생성한 파일 삭제
+        os.remove(file_name)
+    db.commit()
+
+    return {"success_count": success_count, "failure_count": failure_count,
+            "failure_detail": url}
 
 
 @router.get('', status_code=status.HTTP_200_OK)
