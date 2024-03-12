5f5f38b05d16b9ff14f1336e679d0dd8eef87092
david-innosonian <david@innosonian.com>
2024-03-12 10:59:39 +0900

    refactor convert schema to model

--- apis/training_programs.py
+++ apis/training_programs.py
@@ -12,21 +12,7 @@ router = APIRouter(prefix='/training-programs')
 
 @router.post('', response_model=CreateResponseSchema)
 async def create_training_program(data: CreateRequestSchema, db: Session = Depends(get_db)):
-    training_program = TrainingProgram(
-        title=data.title,
-        manikin_type=data.manikin_type,
-        training_type=data.training_type,
-        feedback_type=data.feedback_type,
-        training_mode=data.training_mode,
-        duration=data.duration,
-        compression_limit=data.compression_limit,
-        cycle_limit=data.cycle_limit,
-        ventilation_limit=data.ventilation_limit,
-        per_compression=data.per_compression,
-        per_ventilation=data.per_ventilation,
-        cpr_guideline_id=data.cpr_guideline_id if data.cpr_guideline_id else 2,
-        organization_id=data.organization_id
-    )
+    training_program = data.convert_to_model
 
     db.add(training_program)
     db.commit()
--- schema/training_program.py
+++ schema/training_program.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 
+from models import TrainingProgram
+
 
 class CreateRequestSchema(BaseModel):
     title: str | None = None
@@ -15,6 +17,24 @@ class CreateRequestSchema(BaseModel):
     per_ventilation: int | None = None
     organization_id: int
     cpr_guideline_id: int
+
+    @property
+    def convert_to_model(self):
+        return TrainingProgram(
+            title=self.title,
+            manikin_type=self.manikin_type,
+            training_type=self.training_type,
+            feedback_type=self.feedback_type,
+            training_mode=self.training_mode,
+            duration=self.duration,
+            compression_limit=self.compression_limit,
+            cycle_limit=self.cycle_limit,
+            ventilation_limit=self.ventilation_limit,
+            per_compression=self.per_compression,
+            per_ventilation=self.per_ventilation,
+            cpr_guideline_id=self.cpr_guideline_id if self.cpr_guideline_id else 2,
+            organization_id=self.organization_id
+        )
 
 
 class CreateResponseSchema(BaseModel):
